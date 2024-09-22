c229d7f4fbf2482e4287340c03d9d3ecdedb80fd
x500x <109746464+x500x@users.noreply.github.com>
2024-09-22 23:58:54 +0800

    更新 downlaodvideo.py

--- downlaodvideo.py
+++ downlaodvideo.py
@@ -7,8 +7,6 @@ Downloadvideo_path="C:\\videos"
 Downloadvideo_name="1.mp4"
 #os.system("you-get https://b23.tv/CstBqCK -i -c .\\c.txt")
 os.system("you-get https://b23.tv/CstBqCK -O 1 -o C:\\videos -c .\\c.txt")
-print(Downloadvideo_path+Downloadvideo_name)
-uploader(Downloadvideo_path+Downloadvideo_name
 os.system("dir C:\\videos /s")
 print(Downloadvideo_path+Downloadvideo_name)
 uploader(Downloadvideo_path+Downloadvideo_name)

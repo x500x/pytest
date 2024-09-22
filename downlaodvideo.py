fd533eec474c6f9e05e7bbf956e3786702ffae66
x500x <109746464+x500x@users.noreply.github.com>
2024-09-22 23:22:09 +0800

    更新 downlaodvideo.py

--- downlaodvideo.py
+++ downlaodvideo.py
@@ -6,7 +6,7 @@ from uplaod import uploader
 #--format=flv1080
 Downloadvideo_path="D:\\a\\pytest\\pytest\\video\\"
 Downloadvideo_name="1.mp4"
-os.system("you-get https://b23.tv/CstBqCK -i")
-os.system("you-get https://b23.tv/CstBqCK -O 1 -o "+Downloadvideo_path" -c .\\c.txt")
+os.system("you-get https://b23.tv/CstBqCK -i -c .\\c.txt")
+os.system("you-get https://b23.tv/CstBqCK -O 1 -o "+Downloadvideo_path+" -c .\\c.txt")
 print(Downloadvideo_path+Downloadvideo_name)
 uploader(Downloadvideo_path+Downloadvideo_name)

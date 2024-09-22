1fa2d69e8e372b1821ec0c8b32bdf6c064e8339c
x500x <109746464+x500x@users.noreply.github.com>
2024-09-22 23:03:16 +0800

    更新 downlaodvideo.py

--- downlaodvideo.py
+++ downlaodvideo.py
@@ -5,6 +5,6 @@ from uplaod import uploader
 
 
 Downloadvideo_path=".\\1.mp4"
-os.system("you-get https://b23.tv/CstBqCK -i"
-#os.system("you-get https://b23.tv/CstBqCK -O 1 -o .\\ -c .\\c.txt")
+os.system("you-get https://b23.tv/CstBqCK -i")
+os.system("you-get https://b23.tv/CstBqCK -O 1 -o .\\ -c .\\c.txt")
 uploader(Downloadvideo_path)

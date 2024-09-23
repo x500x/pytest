bc1373bfac35bdac0a77e5e2dbb30c69739f6504
x500x <109746464+x500x@users.noreply.github.com>
2024-09-23 13:04:00 +0800

    更新 downlaodvideo.py

--- downlaodvideo.py
+++ downlaodvideo.py
@@ -4,10 +4,11 @@ from uplaod import uploader
 #\'\\r\\n\'
 
 Downloadvideo_path="C:\\videos\\"
-Downloadvideo_name="1.mp4"
+Downloadvideo_name="Download_video.mp4"
 os.system("md C:\\videos")
 #os.system("you-get https://b23.tv/CstBqCK -i -c .\\c.txt")
 os.system("you-get https://b23.tv/CstBqCK -O 1 -o C:\\videos -c .\\c.txt --debug")
 os.system("dir C:\\videos")
+os.system("ffmpeg -i C:\\videos\\1[00].mp4 -i C:\\videos\\1[01].mp4 -c:v copy -c:a copy -f mp4 C:\\videos\\Download_video.mp4")
 print(Downloadvideo_path+Downloadvideo_name)
 uploader(Downloadvideo_path+Downloadvideo_name)
